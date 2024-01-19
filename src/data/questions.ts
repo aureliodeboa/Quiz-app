9dee64b5286128314544e2eee9e86feaf5614cc2
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-01-19 11:05:03 -0300

    feat-visualization of question

---
+++ src/data/questions.ts
@@ -0,0 +1,17 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question [] = [
+    {
+        question: "Quem descobriu o Brasil?",
+        options: [
+            'Cristovão Colombo',
+            'Homem de Ferro',
+            'Pedro Alvares Cabral',
+            'Minha Mãe',
+            'Rainha Elisabeth',
+
+        ],
+        answer: 2,
+    }
+
+]
